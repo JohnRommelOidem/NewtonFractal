f3ae0a7bd58ea1fc3092ed8068760de6ae07969f
John Rommel Oidem <johnrommeloidem.prf@gmail.com>
2025-10-15 07:57:22 +0800

    Changed trajectory color to match the root destination

--- src/traj.js
+++ src/traj.js
@@ -1,5 +1,6 @@
 import { uniforms, complexToCanvas, canvasToComplex } from "./state";
 import { drag, select, line } from "d3";
+import { cursorColors } from "./funcUtils";
 
 function complexAdd(z1, z2){
     return [z1[0]+z2[0], z1[1]+z2[1]]
@@ -16,14 +17,8 @@ function complexMult(z1, z2){
     ]
 }
 
+
 function complexDiv(z1, z2){
-    const denominator = z2[0]**2+z2[1]**2;
-    return [
-        (z1[0]*z2[0]+z1[1]*z2[1])/denominator, 
-        (z1[1]*z2[0]-z1[0]*z2[1])/denominator
-    ]
-}
-function complexReciprocal(z2){
     const denominator = z2[0]**2+z2[1]**2;
     return [
         (z1[0]*z2[0]+z1[1]*z2[1])/denominator, 
@@ -47,6 +42,28 @@ function fOverFPrime(z, roots = uniforms.u_roots.value){
     return complexDiv(result, deriv);
 }
 
+function complexDistance(z1, z2){
+    const displacement = complexSubtract(z1, z2);
+    return displacement[0]**2+displacement[1]**2
+}
+
+/*
+function complexReciprocal(z){
+    const denominator = z[0]**2+z[1]**2;
+    return [
+        z[0]/denominator, 
+        -z[1]/denominator
+    ]
+} 
+
+function fOverFPrime(z, roots = uniforms.u_roots.value){
+    let denom = [0, 0];
+    for (let i=0;i<roots.length;i++){
+        denom = complexAdd(denom, complexReciprocal(complexSubtract(z, roots[i])));
+    }
+    return complexReciprocal(denom);
+}*/
+
 function getIterations(z, iterations){
     let points = [z];
     for (let i=0;i<iterations;i++){
@@ -56,26 +73,44 @@ function getIterations(z, iterations){
     return points;
 }
 
+function getNearestColor(z, roots = uniforms.u_roots.value){
+    let nearestIndex = -1;
+    let nearestDistance = Infinity;
+    for (let i = 0; i<roots.length;i++){
+        const currentDistance = complexDistance(z, roots[i])
+        if (nearestDistance>currentDistance){
+            nearestDistance = currentDistance;
+            nearestIndex = i;
+        }
+    }
+    return nearestIndex!==-1?cursorColors[nearestIndex]:"black"
+}
+
 export default function initTraj(svg, drawState){
     const trajGroup = svg.append("g").attr("class", "traj-layer");
     const initialPosition = [0.2, 0.3]
     function renderTraj(){
-        const canvasPoints = getIterations(initialPosition, uniforms.u_iterations.value);
+        let complexPoints = getIterations(initialPosition, uniforms.u_iterations.value);
+        let nearestColor = getNearestColor(complexPoints.at(-1));
         const cursorDragBehavior = drag().on("drag", function(e, d){
                 [d[0], d[1]] = canvasToComplex([e.x, e.y]);
+                complexPoints = getIterations(initialPosition, uniforms.u_iterations.value)
+                nearestColor = getNearestColor(complexPoints.at(-1));
                 select(this)
                     .attr("cx", e.x)
                     .attr("cy", e.y)
-                    .raise();
-                const canvasPoints = getIterations(initialPosition, uniforms.u_iterations.value)
+                    .raise()
+                    .attr("fill", nearestColor)
                 trajGroup.selectAll(".traj-line")
-                    .data([canvasPoints])
+                    .data([complexPoints])
                     .attr("d", line().x(p=>complexToCanvas(p)[0]).y(p=>complexToCanvas(p)[1]))
+                    .attr("stroke", nearestColor)
                 
                 trajGroup.selectAll(".traj-point")
-                    .data(canvasPoints.slice(1))
+                    .data(complexPoints.slice(1))
                     .attr("cx", (d)=>complexToCanvas(d)[0])
                     .attr("cy", (d)=>complexToCanvas(d)[1])
+                    .attr("fill", nearestColor)
                 
             })
         if (!drawState.drawTraj){
@@ -84,18 +119,19 @@ export default function initTraj(svg, drawState){
         }
         
         trajGroup.selectAll(".traj-line")
-            .data([canvasPoints])
+            .data([complexPoints])
             .join("path")
             .attr("class", "traj-line")
             .attr("d", line().x(p=>complexToCanvas(p)[0]).y(p=>complexToCanvas(p)[1]))
-
+            .attr("stroke", nearestColor)
+        
         trajGroup.selectAll(".traj-point")
-            .data(canvasPoints.slice(1))
+            .data(complexPoints.slice(1))
             .join("circle")
             .attr("class", "traj-point")
             .attr("cx", (d)=>complexToCanvas(d)[0])
             .attr("cy", (d)=>complexToCanvas(d)[1])
-            .attr("fill", "black")
+            .attr("fill", nearestColor)
             
         trajGroup.selectAll(".init-point")
             .data([initialPosition])
@@ -103,7 +139,7 @@ export default function initTraj(svg, drawState){
             .attr("class", "init-point")
             .attr("cx", (d)=>complexToCanvas(d)[0])
             .attr("cy", (d)=>complexToCanvas(d)[1])
-            .attr("fill", "black")
+            .attr("fill", nearestColor)
             .call(cursorDragBehavior)
     }
     return renderTraj
